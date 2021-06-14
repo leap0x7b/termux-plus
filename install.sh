1b7db20c6edeb196aa5c1fbb5a9c8d00498d5227
Leap of Azzam <sayyid.azzam.mufhadol@gmail.com>
2021-06-14 12:59:06 +0700

    fix

--- install.sh
+++ install.sh
@@ -1,8 +1,7 @@
 #!/data/data/com.termux/files/bin/sh
 export REPO_URL="https://leapofazzam123.github.io/termux-plus"
 echo "[:] Adding Termux Plus..."
-echo "deb [trusted=yes] $REPO_URL termux plus" > $PREFIX/etc/apt/sources.d/termux-plus.list
+echo "deb [trusted=yes] $REPO_URL termux plus" > $PREFIX/etc/apt/sources.lsit.d/termux-plus.list
 echo "[:] Updating APT repository..."
 apt update
 echo "[✓] Succesfully set up Termux Plus. Run 'apt list | grep plus' to list Termux Plus packages"
-
