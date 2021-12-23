12a8a2635e2df96fc66ef16917c01fe05665f3b0
Leap of Azzam <leapofazzam@gmail.com>
2021-12-23 19:31:53 +0700

    🙂🔫👈
    Signed-off-by: Leap of Azzam <leapofazzam@gmail.com>

--- start-builder.sh
+++ start-builder.sh
@@ -81,7 +81,7 @@ fi
 		fi
 	fi
 
-	PACKAGES="$(find "$REPOROOT"/packages -mindepth 1 -maxdepth 1 -type d)"
+	PACKAGES=$(find "$REPOROOT"/packages -mindepth 1 -maxdepth 1 -type d)
 	PACKAGES+=" $(find "$REPOROOT"/x11-packages/packages -mindepth 1 -maxdepth 1 -type d)"
 	echo "[*] Copying packages from './packages' to build environment..."
 	for pkg in ${PACKAGES}; do
